8ba61b52cea31213f0e3733a00631a388aa79e06
Nidhi <nidhigupta.9425@gmail.com>
2018-07-04 13:00:19 +0000

    updated the missing code

--- Reactivespartans/reactiveAppClient/src/components/comments.js
+++ Reactivespartans/reactiveAppClient/src/components/comments.js
@@ -1,4 +1,26 @@
-
+import React, { Component } from 'react';
+
+  class CommentBox extends Component {
+    
+    handleCommentSubmit(comment) {
+      this.props.data.push(comment);
+      var comments = this.state.data;
+      var newComments = comments.concat([comment]);
+      this.setState({data: newComments});
+    }
+
+    render() {
+      return (
+        <div className="comment-box">
+           <CommentForm data={this.props.data} onCommentSubmit={this.handleCommentSubmit.bind(this)} />
+          <CommentList data={this.props.data} />
+        </div>
+      );
+    }
+  };
+
+  class CommentList extends Component {
+  
     render() {
       return (
         <div className="comment-list">
@@ -30,7 +52,36 @@
         return;
       }
 
+      this.props.onCommentSubmit({author: authorVal, text: textVal});
+      e.target[0].value = '';
+      e.target[1].value = '';
+      return;
+    }
 
+    render() {
+      return(
+        <form className="comment-form form-group" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <span className="input-group-addon">Name</span>
+            <input type="text" placeholder="Your name" className="form-control" />
+          </div>          
+          <div className="form-group">
+            <span className="input-group-addon">Comment</span>
+            <input type="text" placeholder="Say something..." className="form-control" />
+          </div>
+          <input type="submit" value="Post" className="btn btn-primary" />
+        </form>
+      );
+    }
+  };
+
+  class Comment extends Component {
+  
+    render() {
+      return (
+        <div className="comment">
+          <h2 className="author">{this.props.author}</h2>
+          {this.props.text}
         </div>
       );
     }
